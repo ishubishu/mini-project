e11eacde136d01551c63e9dcc37d82177ee48325
khuss2432 <60210511+khuss2432@users.noreply.github.com>
2022-07-16 00:06:25 +0530

    Create registration.php

---
+++ userregistration/registration.php
@@ -0,0 +1,21 @@
+<?php
+
+session_start();
+header('location: login.html');
+
+$con=mysqli_connect("localhost","root","");
+mysqli_select_db($con,'userregistration');
+$name=$_POST['user'];
+$pass=$_POST['password'];
+
+$s=" select * from usertable where name='$name'";
+$result= mysqli_query($con, $s);
+$num= mysqli_num_rows($result);
+if($num==1){
+    echo"Username Already Taken";
+}else{
+    $reg="insert into usertable(name, password) value ('$name', '$pass')";
+    mysqli_query($con, $reg);
+    echo"Registration Successful";
+}
+?>
